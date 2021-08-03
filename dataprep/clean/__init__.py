49b3da28c4bdfd35c29887e8384e41a4cee00fd6
Noir Tree <2515744793@qq.com>
2021-08-03 23:25:56 +0800

    feat(clean): add multiple clean functions for number types

--- dataprep/clean/__init__.py
+++ dataprep/clean/__init__.py
@@ -29,6 +29,206 @@ from .clean_df import clean_df
 
 from .clean_text import clean_text, default_text_pipeline
 
+from .clean_au_abn import clean_au_abn, validate_au_abn
+
+from .clean_au_acn import clean_au_acn, validate_au_acn
+
+from .clean_au_tfn import clean_au_tfn, validate_au_tfn
+
+from .clean_be_iban import clean_be_iban, validate_be_iban
+
+from .clean_be_vat import clean_be_vat, validate_be_vat
+
+from .clean_bg_egn import clean_bg_egn, validate_bg_egn
+
+from .clean_bg_pnf import clean_bg_pnf, validate_bg_pnf
+
+from .clean_bg_vat import clean_bg_vat, validate_bg_vat
+
+from .clean_br_cnpj import clean_br_cnpj, validate_br_cnpj
+
+from .clean_br_cpf import clean_br_cpf, validate_br_cpf
+
+from .clean_by_unp import clean_by_unp, validate_by_unp
+
+from .clean_ca_bn import clean_ca_bn, validate_ca_bn
+
+from .clean_ca_sin import clean_ca_sin, validate_ca_sin
+
+from .clean_ch_esr import clean_ch_esr, validate_ch_esr
+
+from .clean_ch_ssn import clean_ch_ssn, validate_ch_ssn
+
+from .clean_ch_uid import clean_ch_uid, validate_ch_uid
+
+from .clean_ch_vat import clean_ch_vat, validate_ch_vat
+
+from .clean_cl_rut import clean_cl_rut, validate_cl_rut
+
+from .clean_cn_ric import clean_cn_ric, validate_cn_ric
+
+from .clean_cn_uscc import clean_cn_uscc, validate_cn_uscc
+
+from .clean_co_nit import clean_co_nit, validate_co_nit
+
+from .clean_cr_cpf import clean_cr_cpf, validate_cr_cpf
+
+from .clean_cr_cpj import clean_cr_cpj, validate_cr_cpj
+
+from .clean_cr_cr import clean_cr_cr, validate_cr_cr
+
+from .clean_cu_ni import clean_cu_ni, validate_cu_ni
+
+from .clean_cy_vat import clean_cy_vat, validate_cy_vat
+
+from .clean_cz_dic import clean_cz_dic, validate_cz_dic
+
+from .clean_cz_rc import clean_cz_rc, validate_cz_rc
+
+from .clean_de_handelsregisternummer import (
+    clean_de_handelsregisternummer,
+    validate_de_handelsregisternummer,
+)
+
+from .clean_de_idnr import clean_de_idnr, validate_de_idnr
+
+from .clean_de_stnr import clean_de_stnr, validate_de_stnr
+
+from .clean_de_vat import clean_de_vat, validate_de_vat
+
+from .clean_de_wkn import clean_de_wkn, validate_de_wkn
+
+from .clean_dk_cpr import clean_dk_cpr, validate_dk_cpr
+
+from .clean_dk_cvr import clean_dk_cvr, validate_dk_cvr
+
+from .clean_do_cedula import clean_do_cedula, validate_do_cedula
+
+from .clean_do_ncf import clean_do_ncf, validate_do_ncf
+
+from .clean_do_rnc import clean_do_rnc, validate_do_rnc
+
+from .clean_ec_ci import clean_ec_ci, validate_ec_ci
+
+from .clean_ec_ruc import clean_ec_ruc, validate_ec_ruc
+
+from .clean_ee_ik import clean_ee_ik, validate_ee_ik
+
+from .clean_ee_kmkr import clean_ee_kmkr, validate_ee_kmkr
+
+from .clean_ee_registrikood import clean_ee_registrikood, validate_ee_registrikood
+
+from .clean_es_ccc import clean_es_ccc, validate_es_ccc
+
+from .clean_es_cif import clean_es_cif, validate_es_cif
+
+from .clean_es_cups import clean_es_cups, validate_es_cups
+
+from .clean_es_dni import clean_es_dni, validate_es_dni
+
+from .clean_es_iban import clean_es_iban, validate_es_iban
+
+from .clean_es_nie import clean_es_nie, validate_es_nie
+
+from .clean_es_nif import clean_es_nif, validate_es_nif
+
+from .clean_es_referenciacatastral import (
+    clean_es_referenciacatastral,
+    validate_es_referenciacatastral,
+)
+
+from .clean_eu_at_02 import clean_eu_at_02, validate_eu_at_02
+
+from .clean_eu_banknote import clean_eu_banknote, validate_eu_banknote
+
+from .clean_eu_eic import clean_eu_eic, validate_eu_eic
+
+from .clean_eu_nace import clean_eu_nace, validate_eu_nace
+
+from .clean_eu_vat import clean_eu_vat, validate_eu_vat
+
+from .clean_fi_alv import clean_fi_alv, validate_fi_alv
+
+from .clean_fi_associationid import (
+    clean_fi_associationid,
+    validate_fi_associationid,
+)
+
+from .clean_fi_hetu import clean_fi_hetu, validate_fi_hetu
+
+from .clean_fi_veronumero import (
+    clean_fi_veronumero,
+    validate_fi_veronumero,
+)
+
+from .clean_fi_ytunnus import clean_fi_ytunnus, validate_fi_ytunnus
+
+from .clean_fr_nif import clean_fr_nif, validate_fr_nif
+
+from .clean_fr_nir import clean_fr_nir, validate_fr_nir
+
+from .clean_fr_siren import clean_fr_siren, validate_fr_siren
+
+from .clean_fr_siret import clean_fr_siret, validate_fr_siret
+
+from .clean_fr_tva import clean_fr_tva, validate_fr_tva
+
+from .clean_gb_nhs import clean_gb_nhs, validate_gb_nhs
+
+from .clean_gb_sedol import clean_gb_sedol, validate_gb_sedol
+
+from .clean_gb_upn import clean_gb_upn, validate_gb_upn
+
+from .clean_gb_utr import clean_gb_utr, validate_gb_utr
+
+from .clean_gb_vat import clean_gb_vat, validate_gb_vat
+
+from .clean_gr_amka import clean_gr_amka, validate_gr_amka
+
+from .clean_gr_vat import clean_gr_vat, validate_gr_vat
+
+from .clean_gt_nit import clean_gt_nit, validate_gt_nit
+
+from .clean_hr_oib import clean_hr_oib, validate_hr_oib
+
+from .clean_hu_anum import clean_hu_anum, validate_hu_anum
+
+from .clean_id_npwp import clean_id_npwp, validate_id_npwp
+
+from .clean_ie_pps import clean_ie_pps, validate_ie_pps
+
+from .clean_ie_vat import clean_ie_vat, validate_ie_vat
+
+from .clean_il_hp import clean_il_hp, validate_il_hp
+
+from .clean_il_idnr import clean_il_idnr, validate_il_idnr
+
+from .clean_in_aadhaar import clean_in_aadhaar, validate_in_aadhaar
+
+from .clean_in_pan import clean_in_pan, validate_in_pan
+
+from .clean_is_kennitala import clean_is_kennitala, validate_is_kennitala
+
+from .clean_is_vsk import clean_is_vsk, validate_is_vsk
+
+from .clean_it_aic import clean_it_aic, validate_it_aic
+
+from .clean_it_codicefiscale import clean_it_codicefiscale, validate_it_codicefiscale
+
+from .clean_it_iva import clean_it_iva, validate_it_iva
+
+from .clean_jp_cn import clean_jp_cn, validate_jp_cn
+
+from .clean_kr_brn import clean_kr_brn, validate_kr_brn
+
+from .clean_kr_rrn import clean_kr_rrn, validate_kr_rrn
+
+from .clean_li_peid import clean_li_peid, validate_li_peid
+
+from .clean_lt_asmens import clean_lt_asmens, validate_lt_asmens
+
+from .clean_lt_pvm import clean_lt_pvm, validate_lt_pvm
+
 
 __all__ = [
     "clean_lat_long",
@@ -54,4 +254,192 @@ __all__ = [
     "clean_df",
     "clean_text",
     "default_text_pipeline",
+    "clean_au_abn",
+    "validate_au_abn",
+    "clean_au_acn",
+    "validate_au_acn",
+    "clean_au_tfn",
+    "validate_au_tfn",
+    "clean_be_iban",
+    "validate_be_iban",
+    "clean_be_vat",
+    "validate_be_vat",
+    "clean_bg_egn",
+    "validate_bg_egn",
+    "clean_bg_pnf",
+    "validate_bg_pnf",
+    "clean_bg_vat",
+    "validate_bg_vat",
+    "clean_br_cnpj",
+    "validate_br_cnpj",
+    "clean_br_cpf",
+    "validate_br_cpf",
+    "clean_by_unp",
+    "validate_by_unp",
+    "clean_ca_bn",
+    "validate_ca_bn",
+    "clean_ca_sin",
+    "validate_ca_sin",
+    "clean_ch_esr",
+    "validate_ch_esr",
+    "clean_ch_ssn",
+    "validate_ch_ssn",
+    "clean_ch_uid",
+    "validate_ch_uid",
+    "clean_ch_vat",
+    "validate_ch_vat",
+    "clean_cl_rut",
+    "validate_cl_rut",
+    "clean_cn_ric",
+    "validate_cn_ric",
+    "clean_cn_uscc",
+    "validate_cn_uscc",
+    "clean_co_nit",
+    "validate_co_nit",
+    "clean_cr_cpf",
+    "validate_cr_cpf",
+    "clean_cr_cpj",
+    "validate_cr_cpj",
+    "clean_cr_cr",
+    "validate_cr_cr",
+    "clean_cu_ni",
+    "validate_cu_ni",
+    "clean_cy_vat",
+    "validate_cy_vat",
+    "clean_cz_dic",
+    "validate_cz_dic",
+    "clean_cz_rc",
+    "validate_cz_rc",
+    "clean_de_handelsregisternummer",
+    "validate_de_handelsregisternummer",
+    "clean_de_idnr",
+    "validate_de_idnr",
+    "clean_de_stnr",
+    "validate_de_stnr",
+    "clean_de_vat",
+    "validate_de_vat",
+    "clean_de_wkn",
+    "validate_de_wkn",
+    "clean_dk_cpr",
+    "validate_dk_cpr",
+    "clean_dk_cvr",
+    "validate_dk_cvr",
+    "clean_do_cedula",
+    "validate_do_cedula",
+    "clean_do_ncf",
+    "validate_do_ncf",
+    "clean_do_rnc",
+    "validate_do_rnc",
+    "clean_ec_ci",
+    "validate_ec_ci",
+    "clean_ec_ruc",
+    "validate_ec_ruc",
+    "clean_ee_ik",
+    "validate_ee_ik",
+    "clean_ee_kmkr",
+    "validate_ee_kmkr",
+    "clean_ee_registrikood",
+    "validate_ee_registrikood",
+    "clean_es_ccc",
+    "validate_es_ccc",
+    "clean_es_cif",
+    "validate_es_cif",
+    "clean_es_cups",
+    "validate_es_cups",
+    "clean_es_dni",
+    "validate_es_dni",
+    "clean_es_iban",
+    "validate_es_iban",
+    "clean_es_nie",
+    "validate_es_nie",
+    "clean_es_nif",
+    "validate_es_nif",
+    "clean_es_referenciacatastral",
+    "validate_es_referenciacatastral",
+    "clean_eu_at_02",
+    "validate_eu_at_02",
+    "clean_eu_banknote",
+    "validate_eu_banknote",
+    "clean_eu_eic",
+    "validate_eu_eic",
+    "clean_eu_nace",
+    "validate_eu_nace",
+    "clean_eu_vat",
+    "validate_eu_vat",
+    "clean_fi_alv",
+    "validate_fi_alv",
+    "clean_fi_associationid",
+    "validate_fi_associationid",
+    "clean_fi_hetu",
+    "validate_fi_hetu",
+    "clean_fi_veronumero",
+    "validate_fi_veronumero",
+    "clean_fi_ytunnus",
+    "validate_fi_ytunnus",
+    "clean_fr_nif",
+    "validate_fr_nif",
+    "clean_fr_nir",
+    "validate_fr_nir",
+    "clean_fr_siren",
+    "validate_fr_siren",
+    "clean_fr_siret",
+    "validate_fr_siret",
+    "clean_fr_tva",
+    "validate_fr_tva",
+    "clean_gb_nhs",
+    "validate_gb_nhs",
+    "clean_gb_sedol",
+    "validate_gb_sedol",
+    "clean_gb_upn",
+    "validate_gb_upn",
+    "clean_gb_utr",
+    "validate_gb_utr",
+    "clean_gb_vat",
+    "validate_gb_vat",
+    "clean_gr_amka",
+    "validate_gr_amka",
+    "clean_gr_vat",
+    "validate_gr_vat",
+    "clean_gt_nit",
+    "validate_gt_nit",
+    "clean_hr_oib",
+    "validate_hr_oib",
+    "clean_hu_anum",
+    "validate_hu_anum",
+    "clean_id_npwp",
+    "validate_id_npwp",
+    "clean_ie_pps",
+    "validate_ie_pps",
+    "clean_ie_vat",
+    "validate_ie_vat",
+    "clean_il_hp",
+    "validate_il_hp",
+    "clean_il_idnr",
+    "validate_il_idnr",
+    "clean_in_aadhaar",
+    "validate_in_aadhaar",
+    "clean_in_pan",
+    "validate_in_pan",
+    "clean_is_kennitala",
+    "validate_is_kennitala",
+    "clean_is_vsk",
+    "validate_is_vsk",
+    "clean_it_aic",
+    "validate_it_aic",
+    "clean_it_codicefiscale",
+    "validate_it_codicefiscale",
+    "clean_it_iva",
+    "validate_it_iva",
+    "clean_jp_cn",
+    "validate_jp_cn",
+    "clean_kr_brn",
+    "validate_kr_brn",
+    "clean_kr_rrn",
+    "validate_kr_rrn",
+    "clean_li_peid",
+    "validate_li_peid",
+    "clean_lt_asmens",
+    "validate_lt_asmens",
+    "clean_lt_pvm",
+    "validate_lt_pvm",
 ]
