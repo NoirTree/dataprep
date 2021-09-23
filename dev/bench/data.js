a367d52b4a4dd1fe7893b66b237551f521fc80a6
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 02:46:15 +0000

    add DataPrep.EDA Benchmarks (pytest) benchmark result for 9f6f5b2c033f0c9346080f8211d32ea249adb1b5

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632365174293,
+  "repoUrl": "https://github.com/NoirTree/dataprep",
+  "entries": {
+    "DataPrep.EDA Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "jlpengcs@gmail.com",
+            "name": "Jinglin Peng",
+            "username": "jinglinpeng"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "9f6f5b2c033f0c9346080f8211d32ea249adb1b5",
+          "message": "Merge pull request #672 from sfu-db/feat/10_clean_functions_1\n\nfeat(clean): add another 10 clean functions for number types",
+          "timestamp": "2021-09-20T23:11:31-07:00",
+          "tree_id": "ac0f914b256514cdae24c058cc598868020b7bdc",
+          "url": "https://github.com/NoirTree/dataprep/commit/9f6f5b2c033f0c9346080f8211d32ea249adb1b5"
+        },
+        "date": 1632365172865,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "dataprep/tests/benchmarks/eda.py::test_create_report",
+            "value": 0.19622158287381344,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0418119918175145",
+            "extra": "mean: 5.096279345800008 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
